0a3c49f8d013f881d9c38d3a13dae6ab77181d32
fatosidrizi <fatos.idrizi@gmail.com>
2023-01-05 20:38:36 +0100

    added product-list component

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  pageTitle = 'Product List'
   constructor() { }
 
   ngOnInit(): void {
