a27e66cab9201e0cfb7c5c76673365953d659d3e
fatosidrizi <fatos.idrizi@gmail.com>
2023-01-05 22:38:08 +0100

    show/hide product image

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -6,30 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-
   pageTitle = 'Product List'
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
+  listFilter = 'cart';
   products: any[] = [
     {
-      "productId": 2,
+      "productId": 1,
       "productName": "Hp-ProBook",
-      "productCode": "Hp-ProBook-1214",
+      "productCode": "Pro-book-770",
       "releaseDate": "March 18, 2022",
       "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, fuga. Itaque, provident.",
-      "price": 700.00,
+      "price": 700,
       "starRating": 4.2,
       "imgUrl": "assets/images/hp.jpg"
     },
     {
       "productId": 2,
       "productName": "Dell XPS",
-      "productCode": "Dell-Xps-13",
+      "productCode": "Dell-xps-13",
       "releaseDate": "April 22, 2022",
       "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, fuga. Itaque, provident.",
-      "price": 1300.00,
+      "price": 1300,
       "starRating": 4.6,
       "imgUrl": "assets/images/dell.jpg"
     },
+    {
+      "productId": 3,
+      "productName": "Apple Macbook Pro",
+      "productCode": "Mac-pro-14",
+      "releaseDate": "September 24, 2022",
+      "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus, fuga. Itaque, provident.",
+      "price": 1300,
+      "starRating": 4.6,
+      "imgUrl": "assets/images/dell.jpg"
+    }
   ]
+
+  toggleImage():void {
+    this.showImage = !this.showImage;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
